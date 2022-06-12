6182e5e76923f6b10e42c732c83a9ecbd50389cc
Julian Schmuckli <julian@schmuckli.net>
2022-06-12 18:15:40 +0200

    Updated folder structure

--- src/setup/Structure.php
+++ src/setup/Structure.php
@@ -3,6 +3,10 @@ namespace Julianschmuckli\Restwork;
 
 class Structure {
     public static function setup() {
+        echo "Setup the folder structure.";
         mkdir("./dao", 0777, true);
+        mkdir("./routes", 0777, true);
+        mkdir("./services", 0777, true);
+        mkdir("./entities", 0777, true);
     }
 }
